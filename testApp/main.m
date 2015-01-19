a55860814b1334c70c96a84e47621ae189385b1b
Spencer Reid <1@Macintosh.local>
2015-01-19 10:57:20 +0200

    Initial Commit

---
+++ testApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testApp
+//
+//  Created by Pavel Zagorskyy on 19.01.15.
+//  Copyright (c) 2015 zagorskyy.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
